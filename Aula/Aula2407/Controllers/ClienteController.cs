74f21b56c754f946a2a977093cf3c9cd1b2eafd8
Alexandre Dattilio <aledattilio@hotmail.com>
2024-07-26 17:01:36 -0300

    subindo aula 26/07 asp.net

---
+++ Aula/Aula2407/Controllers/ClienteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2407.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+    }
+}
+
